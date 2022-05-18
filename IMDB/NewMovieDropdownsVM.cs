874e6b29f3866fb45d7a3769b599d9f4617006c1
RoyaEhab <102411800+RoyaEhab@users.noreply.github.com>
2022-05-18 04:32:54 +0200

    Add files via upload

---
+++ IMDB/NewMovieDropdownsVM.cs
@@ -0,0 +1,24 @@
+﻿using IMDB.Models;
+using System.Collections.Generic;
+
+
+namespace IMDB.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Directors = new List<Director>();
+           
+            Actors = new List<Actor>();
+
+            Movies = new List<Movie>();
+        }
+
+        public List<Director> Directors { get; set; }
+       
+        public List<Actor> Actors { get; set; }
+
+        public List<Movie> Movies { get; set; }
+    }
+}
---
+++ IMDB/RegisterVM.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMDB.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full name")]
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+
+
+        [Display(Name = "image URL")]
+        public string ImageURL { get; set; }
+
+
+        [Display(Name = "age")]
+        public int age { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        //
+        //public int Fav_DirectorID { get; set; }
+        //public int Fav_ActorID { get; set; }
+        //public int Fav_MovieID { get; set; }
+    }
+}
